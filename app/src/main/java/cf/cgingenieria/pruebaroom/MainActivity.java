b5f3ab9aa4c22d9a50399475fbe9ee3df37c5ee8
Carlos <93cgutierrez@gmail.com>
2021-02-07 14:34:31 -0500

    se implementa UI con binding

--- app/src/main/java/cf/cgingenieria/pruebaroom/MainActivity.java
+++ app/src/main/java/cf/cgingenieria/pruebaroom/MainActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import cf.cgingenieria.pruebaroom.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonCreated.setOnClickListener(view -> {
+
+        });
+        binding.buttonRead.setOnClickListener(view -> {
+
+        });
+        binding.buttonUpdate.setOnClickListener(view -> {
+
+        });
+        binding.buttonDelete.setOnClickListener(view -> {
+
+        });
     }
 }
