f91eacbfd48639e2d736754811ac0b44d6a8080d
Carlos <93cgutierrez@gmail.com>
2021-02-07 15:41:00 -0500

    Se implementa el update y delete

--- app/src/main/java/cf/cgingenieria/pruebaroom/MainActivity.java
+++ app/src/main/java/cf/cgingenieria/pruebaroom/MainActivity.java
@@ -1,11 +1,9 @@
 package cf.cgingenieria.pruebaroom;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
-import androidx.room.RoomDatabase;
-
 import android.os.Bundle;
 import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -15,12 +13,10 @@ import cf.cgingenieria.pruebaroom.databinding.ActivityMainBinding;
 import cf.cgingenieria.pruebaroom.model.Person;
 import cf.cgingenieria.pruebaroom.model.PersonDatabase;
 
-import static java.security.AccessController.getContext;
-
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getCanonicalName();
     private ActivityMainBinding binding;
-    private PersonDatabase personDatabase = AppBase.getPersonDatabase();
+    private final PersonDatabase personDatabase = AppBase.getPersonDatabase();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,17 +37,35 @@ public class MainActivity extends AppCompatActivity {
                         .show();
             }).start();
         });
-        binding.buttonRead.setOnClickListener(view -> {
+        binding.buttonRead.setOnClickListener(view -> new Thread(() -> {
+            List<Person> personList = personDatabase.personDao().getAllPerson();
+            Log.d(TAG, "onCreate: personListSize:: " + personList.size());
+        }).start());
+        binding.buttonUpdate.setOnClickListener(view -> {
+            //Realizar operacion bd en hilos secundarios
             new Thread(() -> {
-                List<Person> personList = personDatabase.personDao().getAllPerson();
-                Log.d(TAG, "onCreate: personListSize:: " + personList.size());
+                int idUpdate = 1;
+                Person person = new Person(idUpdate, "PEPE", "PEREZ", "Calle falsa 123 [ACT] ");
+                long id = personDatabase
+                        .personDao()
+                        .updatePerson(person);
+                Log.d(TAG, "onClick: PersonListUpdate:: " + idUpdate + " id:: " + id);
+                Snackbar.make(view, "PersonListUpdate:: " + idUpdate + " id:: " + id, Snackbar.LENGTH_LONG)
+                        .show();
             }).start();
         });
-        binding.buttonUpdate.setOnClickListener(view -> {
-
-        });
         binding.buttonDelete.setOnClickListener(view -> {
-
+            //Realizar operacion bd en hilos secundarios
+            new Thread(() -> {
+                int idDelete = 2;
+                Person person = new Person(idDelete, "PEPE", "PEREZ", "Calle falsa 123 [ACT] ");
+                long id = personDatabase
+                        .personDao()
+                        .deletePerson(person);
+                Log.d(TAG, "onClick: PersonDelete:: " + idDelete + " id:: " + id);
+                Snackbar.make(view, "PersonDelete:: " + idDelete + " id:: " + id, Snackbar.LENGTH_LONG)
+                        .show();
+            }).start();
         });
     }
 }
